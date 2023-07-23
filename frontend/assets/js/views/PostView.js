42347f7f155e586bf08ab679243c1fcc53717d58
Egon Saks <egonsaks@gmail.com>
2023-07-23 23:48:05 +0300

    Clean up changes

--- frontend/assets/js/views/PostView.js
+++ frontend/assets/js/views/PostView.js
@@ -1,8 +1,6 @@
 import { fetchPosts } from "../api/api.js";
 import { createPostComponent } from "../components/post.js";
-
 export async function PostView() {
-  // Extract the id from the URL
   console.log("Calling the PostView from url");
   const urlParts = window.location.href.split("/");
   const id = urlParts[urlParts.length - 1];
@@ -13,17 +11,34 @@ export async function PostView() {
   // Find the post with matching id
   const singlePost = data.find((post) => post.id === id);
 
+  // Try to get the existing contentContainer
+  let contentContainer = document.getElementById("content-container");
+
+  // If contentContainer doesn't exist, create it and append it to #app
+  if (!contentContainer) {
+    contentContainer = document.createElement("div");
+    contentContainer.setAttribute("id", "content-container");
+
+    // Append contentContainer to #app element along with the pageTitle
+    const pageTitle = document.createElement("h1");
+    pageTitle.classList.add("page-title");
+    pageTitle.textContent = "Public room v1.0";
+
+    // Append pageTitle and contentContainer to #app element
+    const root = document.querySelector("#app");
+    root.append(pageTitle, contentContainer);
+  }
+
   // Clear the existing posts
-  const contentContainer = document.getElementById("content-container");
   contentContainer.innerHTML = "";
 
   // If there is a matching post, create the post component for the single post
   if (singlePost) {
     const postComponent = createPostComponent(singlePost);
-    contentContainer.append(postComponent);
+    contentContainer.appendChild(postComponent);
   } else {
     const errorMessage = document.createElement("p");
     errorMessage.textContent = "No post found.";
-    contentContainer.append(errorMessage);
+    contentContainer.appendChild(errorMessage);
   }
 }
